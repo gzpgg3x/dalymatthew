3f199343f5038c69c0445c1bb1a82e02f9c1dd06
Forrest Pan <gzpgg3x@yahoo.com>
2013-04-19 00:19:21 -0400

    PaginatorWorks

---
+++ DjangoBlog/DjangoBlog/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+# from blogengine.views import getPosts
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'DjangoBlog.views.home', name='home'),
+    # url(r'^DjangoBlog/', include('DjangoBlog.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    # url(r'', 'blogengine.views.getRecentPosts'),
+    url(r'^$', 'blogengine.views.getPosts'),
+    url(r'^(?P<selected_page>\d+)/?$', 'blogengine.views.getPosts'),
+    # url(r'^(?P<postSlug>[-a-zA-Z0-9]+)/?$', 'blogengine.views.getPosts'),
+)
